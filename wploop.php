7a6c2c020ffe3fa1910b6b67bac5356597e4a0d3
Jai Nischal Verma <bob@blogohblog.com>
2015-12-06 03:28:15 +0530

    Default Loop

---
+++ wploop.php
@@ -0,0 +1,20 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+     <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+    
+     <div class="pagemeta"><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></div>
+    
+     <div class="entry">
+      <?php if(get_the_post_thumbnail()) { ?> <div class="alignleft"><?php the_post_thumbnail('thumbnail'); ?></div> <?php } ?>
+      <?php the_excerpt(); ?>
+     </div>
+    
+     <div class="pagemeta">Posted in <?php the_category(', '); ?></div>
+     
+ </div> <!--post -->
+
+
+ <?php endwhile;?>
+ <?php endif; ?>
